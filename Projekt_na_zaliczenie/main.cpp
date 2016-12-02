34b7e762456b977f23a6adb8ded933f63436bf7a
Mateusz Podlasinski <Matt@macbook-pro-mateusz.local>
2016-12-02 11:25:46 +0100

    Initial Commit

---
+++ Projekt_na_zaliczenie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projekt_na_zaliczenie
+//
+//  Created by Mateusz Podlasinski on 02.12.2016.
+//  Copyright © 2016 Mateusz Podlasinski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
